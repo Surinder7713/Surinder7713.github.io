5cc154bc101b2a92d26ae109e53d00cacacfcb77
Surinder7713 <surinderjeetsingh1@dcmail.ca>
2024-08-01 08:35:43 -0400

    alternative text

--- Assignment4/Assignmentpart2.js
+++ Assignment4/Assignmentpart2.js
@@ -5,9 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const fileNames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg',]
+const fileNames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const alternativeNames = {
+    'pic1.jpg': 'Eye',
+    'pic2.jpg': 'Mountains',
+    'pic3.jpg': 'Flowers',
+    'pic4.jpg': 'Egyptian',
+    'pic5.jpg': 'Butterfly'
+};
 
 /* Looping through images */
 
